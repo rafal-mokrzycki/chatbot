18a1bbb1a98563e1fbb32a00084479b14dd327ba
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2023-08-29 11:51:21 +0200

    Refinement + add tests

--- tests/test_preprocessor.py
+++ tests/test_preprocessor.py
@@ -7,149 +7,126 @@ import repackage
 repackage.up()
 from scripts.preprocessor import (
     DOCXPreprocessor,
-    PDFPreprocessor,
-    Preprocessor,
-    TableHandler,
+    add_lines,
+    create_csv_file_if_not_exist,
+    extract_text_from_docx,
+    forward_regexp_search,
+    get_files_in_dir,
+    remove_attachments,
+    remove_preambule,
+    replace_forbidden_chars,
+    replace_whitespaces,
+    split_on_points,
 )
 
 
-def test_search_files_in_dir():
+def test_get_files_in_dir():
     directory = str(Path(__file__).parent.parent.joinpath(r"tests/test_files"))
-    p = Preprocessor(directory)
-    assert len(p.search_files_in_dir(directory)) == 2
+    assert len(get_files_in_dir(directory)) == 3
 
 
-def test_extract_sentences_from_docx():
+def test_extract_text_from_docx():
     directory = str(
         Path(__file__).parent.parent.joinpath(r"tests/test_files/test_file.docx")
     )
-    p = DOCXPreprocessor()
-    result = p.extract_sentences_from_docx(directory)
-    assert (
-        result
-        == "This is a paragraph. This is another paragraph. § 1. This is yet another paragraph. Załącznik nr 1 This is an attachment."
-    )
-
-
-def test_preprocess_docx():
-    directory = str(
-        Path(__file__).parent.parent.joinpath(r"tests/test_files/test_file.docx")
-    )
-    p = DOCXPreprocessor()
-    result = p.preprocess_docx(directory)
-    assert result == ["This is yet another paragraph. This is as point"]
-
-
-def test_remove_attachments():
-    string = "random string Załącznik nr 1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_attachments(string)
-    assert result == "random string "
-
-
-def test_remove_preambule_1():
-    string = "random string § 1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_preambule(string)
-    assert result == "another random string"
-
-
-def test_remove_preambule_2():
-    string = "random string §1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_preambule(string)
-    assert result == "another random string"
-
-
-def test_remove_preambule_3():
-    string = "random string $ 1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_preambule(string)
-    assert result == "another random string"
-
-
-def test_remove_preambule_4():
-    string = "random string $1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_preambule(string)
-    assert result == "another random string"
-
-
-def test_remove_preambule_5():
-    string = "random string @ 1 another random string"
-    p = DOCXPreprocessor()
-    result = p.remove_preambule(string)
-    assert result == string
-
-
-def test_replace_forbidden_chars_1():
-    string = "random string; another random string"
-    p = DOCXPreprocessor()
-    result = p.replace_forbidden_chars(string)
-    assert result == "random string, another random string"
-
-
-def test_replace_forbidden_chars_2():
-    string = "random string; another random string"
-    p = DOCXPreprocessor()
-    result = p.replace_forbidden_chars(string, replace_with="!")
-    assert result == "random string! another random string"
-
-
-def test_replace_forbidden_chars_3():
-    string = "random string? another; random string"
-    p = DOCXPreprocessor()
-    result = p.replace_forbidden_chars(string, forbidden_chars=["?", ";"])
-    assert result == "random string, another, random string"
-
-
-def test_replace_whitespaces():
-    string = "random string    another  random   string"
-    p = DOCXPreprocessor()
-    result = p.replace_whitespaces(string)
-    assert result == "random string another random string"
+    result = extract_text_from_docx(directory)
+    assert result[:25] == "This is a paragraph. This"
 
 
 def test_split_on_points_1():
     string = "random string § 2 another random string"
-    p = DOCXPreprocessor()
-    result = p.split_on_points(string)
+    result = split_on_points(string)
     assert result[0] == "random string another random string"
 
 
 def test_split_on_points_2():
     string = "random string §2 another random string"
-    p = DOCXPreprocessor()
-    result = p.split_on_points(string)
+    result = split_on_points(string)
     assert result[0] == "random string another random string"
 
 
 def test_split_on_points_3():
     string = "random string $3 another random string"
-    p = DOCXPreprocessor()
-    result = p.split_on_points(string)
+    result = split_on_points(string)
     assert result[0] == "random string another random string"
 
 
 def test_split_on_points_4():
     string = "random string $ 14 another random string"
-    p = DOCXPreprocessor()
-    result = p.split_on_points(string)
+    result = split_on_points(string)
     assert result[0] == "random string another random string"
 
 
 def test_split_on_points_5():
     string = "random string 44. another random string"
-    p = DOCXPreprocessor()
-    result = p.split_on_points(string)
+    result = split_on_points(string)
     assert result[0] == "random string"
+
+
+def test_remove_preambule_1():
+    string = "random string § 1 another random string"
+    result = remove_preambule(string)
+    assert result == "another random string"
+
+
+def test_remove_preambule_2():
+    string = "random string §1 another random string"
+    result = remove_preambule(string)
+    assert result == "another random string"
+
+
+def test_remove_preambule_3():
+    string = "random string $ 1 another random string"
+    result = remove_preambule(string)
+    assert result == "another random string"
+
+
+def test_remove_preambule_4():
+    string = "random string $1 another random string"
+    result = remove_preambule(string)
+    assert result == "another random string"
+
+
+def test_remove_preambule_5():
+    string = "random string @ 1 another random string"
+    result = remove_preambule(string)
+    assert result == string
+
+
+def test_remove_attachments():
+    string = "random string Załącznik nr 1 another random string"
+    result = remove_attachments(string)
+    assert result == "random string "
+
+
+def test_replace_forbidden_chars_1():
+    string = "random string; another random string"
+    result = replace_forbidden_chars(string)
+    assert result == "random string, another random string"
+
+
+def test_replace_forbidden_chars_2():
+    string = "random string; another random string"
+    result = replace_forbidden_chars(string, replace_with="!")
+    assert result == "random string! another random string"
+
+
+def test_replace_forbidden_chars_3():
+    string = "random string? another; random string"
+    result = replace_forbidden_chars(string, forbidden_chars=["?", ";"])
+    assert result == "random string, another, random string"
+
+
+def test_replace_whitespaces():
+    string = "random string    another  random   string"
+    result = replace_whitespaces(string)
+    assert result == "random string another random string"
 
 
 def test_forward_regexp_search():
     main_string = "random first string another 3. second random string"
     target_string = "first"
-    p = PDFPreprocessor()
-    result = p.forward_regexp_search(main_string=main_string, target_string=target_string)
+    result = forward_regexp_search(main_string=main_string, target_string=target_string)
     assert result == "string another"
 
 
@@ -166,10 +143,30 @@ def test_jsonize_pdf():
 
 
 def test_add_lines():
-    t = TableHandler("test_table.csv")
+    file_path = "test_table.csv"
     lst = ["random", "string"]
-    t.add_lines(lst)
+    add_lines(lst, file_path)
     with open("test_table.csv", "r") as f:
         lines = f.readlines()
     assert lines == ["sentences\n", "random\n", "string"]
     os.remove("test_table.csv")
+
+
+def test_create_csv_file_if_not_exist():
+    file_path = "test_table.csv"
+    create_csv_file_if_not_exist(file_path)
+    with open("test_table.csv", "r") as f:
+        lines = f.readlines()
+    assert lines == ["sentences\n"]
+    os.remove("test_table.csv")
+
+
+def test_preprocess_docx():
+    directory = str(
+        Path(__file__).parent.parent.joinpath(
+            r"tests/test_files/test_Zarządzenie_file.docx"
+        )
+    )
+    p = DOCXPreprocessor(directory)
+    result = p.preprocess()
+    assert result == ["This is yet another paragraph. This is a point"]
