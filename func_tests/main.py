425940b8ae0987f3b70380427179db753dd5715c
Rafał Mokrzycki <rafalmokrzycki1@gmail.com>
2023-08-30 11:29:50 +0200

    update functional tests

--- func_tests/main.py
+++ func_tests/main.py
@@ -1,14 +1,38 @@
+import os
+
 import repackage
 
 repackage.up()
+from pathlib import Path
+
 from scripts.interface import UserInterface
 
-if __name__ == "__main__":
+
+def main():
     interface = UserInterface()
-    # TODO: add line question;answer\n by creation of file
-    with open(r"func_tests\queries.txt", "r") as q:
-        with open(r"func_tests\answers.csv", "w") as a:
+
+    # file to take queries from
+    queries_file_path = Path(__file__).parent.joinpath("queries.txt")
+
+    # target file to write queries and answers to
+    answers_file_path = Path(__file__).parent.joinpath("answers.csv")
+
+    # target file header
+    header = "question;answer\n"
+
+    # if target file does not exist, create it
+    if not os.path.isfile(answers_file_path):
+        with open(answers_file_path, "w") as f:
+            f.write(header)
+
+    # read queries, perform conversation and write queries and answers into trget file
+    with open(queries_file_path, "r") as q:
+        with open(answers_file_path, "a") as a:
             for query in q.readlines():
                 answer = interface.make_conversation(query=query)
                 result = query.replace("\n", "") + ";" + answer + "\n"
                 a.write(result)
+
+
+if __name__ == "__main__":
+    main()
